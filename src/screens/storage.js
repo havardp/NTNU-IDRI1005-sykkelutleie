d3dd269579fa65aba342807901bfad6ff6176c73
kjetill1122 <31021871+kjetill1122@users.noreply.github.com>
2019-03-20 11:31:11 +0100

    comment

--- src/screens/storage.js
+++ src/screens/storage.js
@@ -66,3 +66,112 @@ export class StorageStatus extends Component {
     }
 
  }
+
+ /*
+ export class BikeDetail extends Component {
+   Allbikes = [];
+   render() {
+     return (
+       <div className="main">
+         <Card title={this.props.match.params.id}>
+           <Table striped bordered hover responsive>
+             <thead>
+               <tr>
+                 <td>Chassis id</td>
+                 <td>Gir</td>
+                 <td>Hjulstørrelse</td>
+                 <td>Rep.dato</td>
+                 <td>Ødelagt</td>
+                 <td>Lokasjon</td>
+                 <td>Tilholdssted</td>
+                 <td>Bagasjebrett</td>
+               </tr>
+             </thead>
+             <tbody>
+               {this.Allbikes.map(bike => (
+                 <tr key={bike.chassis_id}>
+                   <td>{bike.chassis_id}</td>
+                   <td>{bike.gear}</td>
+                   <td>{bike.wheel_size}</td>
+                   <td>{bike.rep_date}</td>
+                   <td>{bike.broken}</td>
+                   <td>{bike.location}</td>
+                   <td>{bike.storage}</td>
+                   <td>{bike.luggage}</td>
+                   <td>
+                     <button>&#9881;</button>
+                   </td>
+                   <td>
+                     <button>&#10004;</button>
+                   </td>
+                 </tr>
+               ))}
+               <tr>
+                 <td>
+                   <button className="btn btn-info btn-lg" onClick={() => history.push('/bike/add')}>&#10010;</button>
+                 </td>
+               </tr>
+             </tbody>
+           </Table>
+         </Card>
+       </div>
+     );
+   }
+
+   mounted() {
+     storageService.getBike(this.props.match.params.id, result => {
+       this.Allbikes = result;
+     });
+   }
+ }
+
+
+ export class EquipmentDetail extends Component {
+   name = {
+     eq_id: ' ',
+     model: ' '
+   };
+   Allequipment = [];
+   render() {
+     return (
+       <div className="main">
+         <Card title={this.props.match.params.id}>
+           <Table striped bordered hover responsive>
+             <thead>
+               <tr>
+                 <td>Utstyrs-id</td>
+                 <td>Modell</td>
+               </tr>
+             </thead>
+             <tbody>
+               {this.Allequipment.map(equipment => (
+                 <tr key={equipment.eq_id}>
+                   <td>{equipment.eq_id}</td>
+                   <td>{equipment.model}</td>
+                   <td>
+                     <button>&#9881;</button>
+                   </td>
+                   <td>
+                     <button>&#10004;</button>
+                   </td>
+                 </tr>
+               ))}
+               <tr>
+                 <td>
+                   <button className="btn btn-info btn-lg">&#10010;</button>
+                 </td>
+               </tr>
+             </tbody>
+           </Table>
+         </Card>
+       </div>
+     );
+   }
+
+   mounted() {
+     storageService.getEquipment(this.props.match.params.id, result => {
+       this.Allequipment = result;
+     });
+   }
+ }
+*/
